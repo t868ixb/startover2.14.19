e2b022fc3fff39dbf63fbb7c8f91120e9c32d3ad
“IBayon“ <IBayon@Enova.com>
2019-02-15 01:20:49 -0600

    fix

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
-
+const bodyParser = require('body-parser')
+//const session = require('express-session')
+const Model = require('./models')
+const dbConnection =  Model.mongoose
+// const MongoStore = require('connect-mongo')(session)
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//IB adding for passport
+const passport = require('passport');
+
+// Sessions --> creates an empty session object, as req.session
+// ib saves the session object to the database
+// app.use(
+// 	session({
+// 		secret: 'struggling-ninja', //pick a random string to make the hash that is generated secure
+// 		store: new MongoStore({ mongooseConnection: dbConnection }),
+// 		resave: false, //required
+// 		saveUninitialized: false //required
+// 	})
+// );
+
 // Define middleware here
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize()); //ib
+app.use(passport.session()) // ib calls the deserializeUser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
