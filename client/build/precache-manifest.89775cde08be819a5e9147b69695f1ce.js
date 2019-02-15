5348d60541daabd4672187463d0df8dbdaf31073
“IBayon“ <IBayon@Enova.com>
2019-02-15 00:11:00 -0600

    add files

---
+++ client/build/precache-manifest.89775cde08be819a5e9147b69695f1ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "94bada2f4ad704ab8575",
+    "url": "/static/js/main.94bada2f.chunk.js"
+  },
+  {
+    "revision": "16693711b03cdb27b87a",
+    "url": "/static/js/1.16693711.chunk.js"
+  },
+  {
+    "revision": "94bada2f4ad704ab8575",
+    "url": "/static/css/main.500af5a4.chunk.css"
+  },
+  {
+    "revision": "215ebb3a5d88b5b9109ee2e3fbc8ddf0",
+    "url": "/index.html"
+  }
+];
